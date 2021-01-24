688186aad7b7e21cac0ad93196cf7b1f91360342
Eviks <hutteau.b@gmail.com>
2021-01-24 22:57:30 +0100

    Generic

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,13 @@
-export declare class EzMap {
-    [x: string]: any | undefined;
-    constructor(obj: Object);
-    map(func: (k: string, v: any, o: EzMap) => any): EzMap;
-    filter(func: (k: string, v: any, o: EzMap) => boolean): EzMap;
-    find(func: (k: string, v: any, o: EzMap) => boolean): string | undefined;
-    reduce<T>(func: (acc: T, k: string, v: any, o: EzMap) => T, initialValue: T): T;
+export declare class EzMap<T> {
+    [k: string]: T | (<U>(func: (this: any, k: string, v: T, o: EzMap<T>) => U) => EzMap<U>) | ((func: (k: string, v: any, o: EzMap<T>) => boolean) => EzMap<T>) | ((func: (k: string, v: any, o: EzMap<T>) => boolean) => string | undefined) | (<U>(func: (acc: U, k: string, v: any, o: EzMap<T>) => U, initialValue: U) => U);
+    constructor(obj: {
+        [k: string]: T;
+    });
+    map<U>(func: (k: string, v: T, o: EzMap<T>) => U): EzMap<U>;
+    filter(func: (k: string, v: any, o: EzMap<T>) => boolean): EzMap<T>;
+    find(func: (k: string, v: any, o: EzMap<T>) => boolean): string | undefined;
+    reduce<U>(func: (acc: U, k: string, v: any, o: EzMap<T>) => U, initialValue: U): U;
 }
-export declare function asMap(obj: Object): EzMap;
+export declare function asMap<T>(obj: {
+    [k: string]: T;
+}): EzMap<T>;
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,43 @@
 
-export class EzMap {
-  [x: string]: any | undefined;
-  constructor(obj: Object) {
+export class EzMap<T> {
+
+  [k: string]: T
+    | (<U>(func: (this: any, k: string, v: T, o: EzMap<T>) => U) => EzMap<U>)
+    | ((func: (k: string, v: any, o: EzMap<T>) => boolean) => EzMap<T>)
+    | ((func: (k: string, v: any, o: EzMap<T>) => boolean) => string | undefined)
+    | (<U>(func: (acc: U, k: string, v: any, o: EzMap<T>) => U, initialValue: U) => U)
+  ;
+
+  constructor(obj: { [k: string]: T }) {
     for (let it in obj) {
-      // @ts-ignore
       this[it] = obj[it];
     }
   }
 
   // Maps an object's values
-  map(func: (k: string, v: any, o: EzMap) => any): EzMap {
-    return new EzMap(Object.fromEntries(Object.entries(this).map(e => [ e[0], func(e[0], e[1], this) ])));
+  map<U>(func: (k: string, v: T, o: EzMap<T>) => U): EzMap<U> {
+    // @ts-ignore
+    return new EzMap<U>(Object.fromEntries(Object.entries(this).map(e => [ e[0], func(e[0], e[1], this) ])));
   }
 
   // Filters an object's entries
-  filter(func: (k: string, v: any, o: EzMap) => boolean): EzMap {
+  filter(func: (k: string, v: any, o: EzMap<T>) => boolean): EzMap<T> {
+    // @ts-ignore
     return new EzMap(Object.fromEntries(Object.entries(this).filter(e => func(e[0], e[1], this) )));
   }
 
   // Finds a key using a predicate. Returns undefined if not found
-  find(func: (k: string, v: any, o: EzMap) => boolean): string | undefined {
+  find(func: (k: string, v: any, o: EzMap<T>) => boolean): string | undefined {
+    // @ts-ignore
     return Object.entries(this).find(e => func(e[0], e[1], this) )?.[0];
   }
 
   // Reduces an object to something else
-  reduce<T>(func: (acc: T, k: string, v: any, o: EzMap) => T, initialValue: T): T {
-    return Object.entries(this).reduce<T>((acc, e) => func(acc, e[0], e[1], this), initialValue);
+  reduce<U>(func: (acc: U, k: string, v: any, o: EzMap<T>) => U, initialValue: U): U {
+    return Object.entries(this).reduce<U>((acc, e) => func(acc, e[0], e[1], this), initialValue);
   }
 }
 
-export function asMap(obj: Object): EzMap {
-  return new EzMap(obj);
+export function asMap<T>(obj: { [k: string]: T }): EzMap<T> {
+  return new EzMap<T>(obj);
 }
